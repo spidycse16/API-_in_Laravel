6b82a630534c0ef79138bdf9074ad3b9f460a816
spidycse16 <spidy0709@gmail.com>
2024-05-15 14:02:24 +0600

    get api

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function show()
+    {
+        $Students=Student::all();
+        return $Students;
+    }
+}
