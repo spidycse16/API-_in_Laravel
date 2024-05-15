9b5d8dc87f6bf3507fcc5f333c24deb088dd08e5
spidycse16 <spidy0709@gmail.com>
2024-05-15 14:18:55 +0600

    user update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,6 +10,19 @@ class StudentController extends Controller
     public function show()
     {
         $Students=Student::all();
-        return $Students;
+        if(count($Students)>0){
+            $response=[
+                count($Students).' users found',
+                'status'=>'1',
+                'data'=> $Students
+            ];
+        }
+        else{
+            $response=[
+                '0 '.'users found',
+                'status'=>'0'
+            ];
+        }
+        return response()->json($response,200);
+        }
     }
-}
